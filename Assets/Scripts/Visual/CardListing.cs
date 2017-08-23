a7312c58ae1da735d2548214e811194bbff6df21
Alarack <jcbelgard@gmail.com>
2017-08-23 03:33:57 -0500

    Working on deckbuilder

---
+++ Assets/Scripts/Visual/CardListing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardListing : MonoBehaviour {
+
+    public DeckBuilder deckBuilder;
+    public CardVisual card;
+    public int cardQuantity;
+
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+}
